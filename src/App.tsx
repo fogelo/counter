7112efa75073bcaf6af93e3897bf37c2bfab3db6
antonorlov <fogelo@yandex.ru>
2022-04-17 19:02:24 +0300

    disabled

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,12 @@ import './App.css';
 
 function App() {
     const [value, setValue] = useState(0)
+    const MAX_VALUE = 5
+
     const increment = () => {
-        setValue(value + 1)
+        if (value < MAX_VALUE) {
+            setValue(value + 1)
+        }
     }
     const reset = () => {
         setValue(0)
@@ -12,10 +16,20 @@ function App() {
     return (
         <div className="App">
             <div className={'counter'}>
-                <div className={'value'}>{value}</div>
+                <div className={`value ${value === MAX_VALUE ? 'maxValue' : ''}`}>{value}</div>
                 <div className={'buttons'}>
-                    <button className={'button'} onClick={increment}>inc</button>
-                    <button className={'button'} onClick={reset}>reset</button>
+                    <button className={'button'}
+                            onClick={increment}
+                            disabled={value === MAX_VALUE}
+                    >
+                        inc
+                    </button>
+                    <button className={'button'}
+                            onClick={reset}
+                            disabled={value === 0}
+                    >
+                        reset
+                    </button>
                 </div>
             </div>
         </div>
