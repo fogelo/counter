2736129f813c0bb18a718867414045c15a95c2cf
antonorlov <fogelo@yandex.ru>
2022-04-17 22:44:37 +0300

    localStorage, setItem, getItem, clear, removeItem

--- src/App.tsx
+++ src/App.tsx
@@ -15,6 +15,26 @@ function App() {
     const reset = () => {
         setValue(0)
     }
+
+    const setToLocalStorageHandler = () => {
+        localStorage.setItem('count', JSON.stringify(value))
+        localStorage.setItem('count2', JSON.stringify(value))
+    }
+    const getFromLocalStorageHandler = () => {
+        const value = localStorage.getItem('count')
+
+        if (value) {
+            setValue(JSON.parse(value))
+        }
+    }
+
+    const clearLocalStorageHandler = () => {
+        localStorage.clear()
+    }
+
+    const removeLocalStorageHandler = () => {
+        localStorage.removeItem('count2')
+    }
     return (
         <div className="App">
             <div className={'counter'}>
@@ -24,6 +44,10 @@ function App() {
                 <div className={'buttons'}>
                     <Button className={'button'} onClick={increment} name={'inc'} disabled={value === MAX_VALUE}/>
                     <Button className={'button'} onClick={reset} name={'reset'} disabled={value === 0}/>
+                    <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
+                    <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
+                    <button onClick={clearLocalStorageHandler}>clearLocalStorageHandler</button>
+                    <button onClick={removeLocalStorageHandler}>removeLocalStorageHandler</button>
                 </div>
             </div>
         </div>
