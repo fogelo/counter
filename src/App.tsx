979e842b69ac21f1ac1457258874dc21d55cfa57
antonorlov <fogelo@yandex.ru>
2022-04-17 23:48:31 +0300

    useEffect, localStorage

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import './App.css';
 import {Button} from './components/Button';
 import {Display} from './components/Display';
@@ -15,26 +15,18 @@ function App() {
     const reset = () => {
         setValue(0)
     }
-
-    const setToLocalStorageHandler = () => {
-        localStorage.setItem('count', JSON.stringify(value))
-        localStorage.setItem('count2', JSON.stringify(value))
-    }
-    const getFromLocalStorageHandler = () => {
+    useEffect(() => {
         const value = localStorage.getItem('count')
-
         if (value) {
-            setValue(JSON.parse(value))
+            let newValue = JSON.parse(value)
+            setValue(newValue)
         }
-    }
+    }, [])
 
-    const clearLocalStorageHandler = () => {
-        localStorage.clear()
-    }
+    useEffect(() => {
+        localStorage.setItem('count', JSON.stringify(value))
+    }, [value])
 
-    const removeLocalStorageHandler = () => {
-        localStorage.removeItem('count2')
-    }
     return (
         <div className="App">
             <div className={'counter'}>
@@ -42,12 +34,8 @@ function App() {
                 <Display className={`value ${value === MAX_VALUE ? 'maxValue' : ''}`} value={value}/>
 
                 <div className={'buttons'}>
-                    <Button className={'button'} onClick={increment} name={'inc'} disabled={value === MAX_VALUE}/>
-                    <Button className={'button'} onClick={reset} name={'reset'} disabled={value === 0}/>
-                    <button onClick={setToLocalStorageHandler}>setToLocalStorage</button>
-                    <button onClick={getFromLocalStorageHandler}>getFromLocalStorage</button>
-                    <button onClick={clearLocalStorageHandler}>clearLocalStorageHandler</button>
-                    <button onClick={removeLocalStorageHandler}>removeLocalStorageHandler</button>
+                    <Button className={'button'} onClick={()=>increment()} name={'inc'} disabled={value === MAX_VALUE}/>
+                    <Button className={'button'} onClick={()=>reset()} name={'reset'} disabled={value === 0}/>
                 </div>
             </div>
         </div>
