09d404a3effb1655c81222a5e704c76156b34f7f
antonorlov <fogelo@yandex.ru>
2022-04-18 19:18:43 +0300

    version 2.0

--- src/App.tsx
+++ src/App.tsx
@@ -1,41 +1,88 @@
-import React, {useEffect, useState} from 'react';
+import React, {ChangeEvent, useEffect, useState} from 'react';
 import './App.css';
 import {Button} from './components/Button';
 import {Display} from './components/Display';
 
 function App() {
-    const [value, setValue] = useState(0)
-    const MAX_VALUE = 5
+    const [value, setValue] = useState(Number(localStorage.getItem('value')) || 0)
+    const [startValue, setStartValue] = useState(Number(localStorage.getItem('startValue')) || 0)
+    const [maxValue, setMaxValue] = useState(Number(localStorage.getItem('maxValue')) || 5)
+    const [settingMode, setSettingMode] = useState(localStorage.getItem('settingMode') === null
+        ? true
+        : localStorage.getItem('settingMode') === 'true'
+    )
 
     const increment = () => {
-        if (value < MAX_VALUE) {
+        if (value < maxValue) {
             setValue(value + 1)
         }
     }
+
     const reset = () => {
         setValue(0)
     }
-    useEffect(() => {
-        const value = localStorage.getItem('count')
-        if (value) {
-            let newValue = JSON.parse(value)
-            setValue(newValue)
-        }
-    }, [])
 
     useEffect(() => {
-        localStorage.setItem('count', JSON.stringify(value))
-    }, [value])
+        localStorage.setItem('value', JSON.stringify(value))
+        localStorage.setItem('startValue', JSON.stringify(startValue))
+        localStorage.setItem('maxValue', JSON.stringify(maxValue))
+        localStorage.setItem('settingMode', JSON.stringify(settingMode))
+    }, [value, startValue, maxValue, settingMode])
+
+    const setValues = () => {
+        if (startValue < maxValue) {
+            setSettingMode(false)
+            setValue(startValue)
+        }
+    }
+
+    const onMaxValueChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setMaxValue(+e.currentTarget.value)
+    }
+
+    const onStartValueChangeHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        setStartValue(+e.currentTarget.value)
+    }
 
     return (
         <div className="App">
-            <div className={'counter'}>
+            <div className={'container'}>
+                <div>
+                    <span>max value: </span>
+                    <input type="number"
+                           value={maxValue}
+                           className={startValue >= maxValue ? 'inputError' : ''}
+                           onFocus={() => setSettingMode(true)}
+                           onChange={onMaxValueChangeHandler}
+                    />
+                </div>
+                <div>
+                    <span>start value: </span>
+                    <input type="number"
+                           value={startValue}
+                           className={startValue >= maxValue ? 'inputError' : ''}
+                           onFocus={() => setSettingMode(true)}
+                           onChange={onStartValueChangeHandler}
+                    />
+                </div>
 
-                <Display className={`value ${value === MAX_VALUE ? 'maxValue' : ''}`} value={value}/>
+                <Button className={'button'}
+                        onClick={setValues}
+                        name={'set'}
+                        disabled={!settingMode || startValue >= maxValue}/>
+            </div>
+
+            <div className={'container'}>
+                <Display className={`value ${value === maxValue ? 'maxValue' : ''}`}
+                         value={value}
+                         settingMode={settingMode}
+                         error={startValue >= maxValue}
+                />
 
                 <div className={'buttons'}>
-                    <Button className={'button'} onClick={()=>increment()} name={'inc'} disabled={value === MAX_VALUE}/>
-                    <Button className={'button'} onClick={()=>reset()} name={'reset'} disabled={value === 0}/>
+                    <Button className={'button'} onClick={increment} name={'inc'}
+                            disabled={settingMode || value === maxValue}/>
+                    <Button className={'button'} onClick={reset} name={'reset'} disabled={settingMode || value === 0}/>
                 </div>
             </div>
         </div>
--- src/components/Display.tsx
+++ src/components/Display.tsx
@@ -1,16 +1,19 @@
 import React from 'react';
+import style from './Display.module.css'
 
 type DisplayPropsType = {
     className: string
     value: number
+    settingMode: boolean
+    error: boolean
 }
 
 export const Display = (props: DisplayPropsType) => {
     return (
-        <>
-            <div className={props.className}>
-                {props.value}
-            </div>
-        </>
+        props.settingMode
+            ? <span>{props.error
+                ? <span className={style.errorMessage}>incorrect values</span>
+                : <span className={style.infoMessage}>enter values and press "set"</span>}</span>
+            : <div className={props.className}>{props.value}</div>
     )
 }
